a133d8de00e20c20d0165f0363e0887d2b6f66c2
teameclipsestaff <teameclipseao@gmail.com>
2022-03-30 22:15:33 -0400

    3 files modified

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ function hamMenu(menu) {
   var element = document.querySelector(".mob-menu-container");
   element.classList.toggle("show");
 }
+
+function closeIcon(x) {
+  x.classList.toggle("close");
+}
